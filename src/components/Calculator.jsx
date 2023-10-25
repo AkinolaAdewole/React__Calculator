28d269e23f0f10ce85073fe7d38bf4f9d3cddc47
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-25 22:18:46 +0100

    commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,6 +3,14 @@ import DigitButton from './DigitButton';
 import OperationButton from './OperationButton';
 import "../style.css"
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate"
+}
+
 const Calculator = () => {
   return (
     <div>Calculator</div>
--- src/components/DigitButton.jsx
+++ src/components/DigitButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { ACTIONS } from './Calculator'
 
-const DigitButton = () => {
+const DigitButton = ({dispatch, digit}) => {
   return (
-    <div>DigiButton</div>
+    <button
+        onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+      >
+        {digit}
+    </button>
   )
 }
 
